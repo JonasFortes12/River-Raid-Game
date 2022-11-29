b614fa8a4b839aafdbf7710ae8753cf04f786052
Jonas Fortes <jonascforte@hotmail.com>
2022-11-29 10:34:27 -0300

    tratamento de colisões

--- js/riverRaid.js
+++ js/riverRaid.js
@@ -1,13 +1,14 @@
-function newElement(tagName, className) {
+function newElement(tagName, className, idName) {
     const element = document.createElement(tagName)
     element.className = className
+    element.id = idName
     return element
 }
 
 const gameArea = document.querySelector('[wm-RiverRaid]')
 
 
-function Barries(){
+function Barries() {
 
     this.bifurcation = [
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
@@ -30,7 +31,7 @@ function Barries(){
         [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
-      ];
+    ];
 
     this.zigzag = [
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
@@ -53,7 +54,7 @@ function Barries(){
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
-      ];
+    ];
 
     this.midle = [
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
@@ -76,7 +77,7 @@ function Barries(){
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
-      ];
+    ];
 
     this.toright = [
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
@@ -99,7 +100,7 @@ function Barries(){
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-      ];
+    ];
 
     this.toleft = [
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
@@ -122,15 +123,15 @@ function Barries(){
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-      ];
+    ];
 
 }
 
-function DrawBarries(barrie){ // recebe a matrix da barreira específica (cenário)
+function DrawBarries(barrie) { // recebe a matrix da barreira específica (cenário)
 
     this.gameContext = document.createElement("div");
     this.gameContext.classList.add("barrieStyle");
-    
+
     this.gameContext.style.position = `absolute`
 
     this.gameContext.style.display = `grid`
@@ -141,18 +142,21 @@ function DrawBarries(barrie){ // recebe a matrix da barreira específica (cenár
     this.gameContext.style.height = "100%";
     this.gameContext.style.top = "-685px";
 
+    let numBarrie = -1
 
-    for(let row in barrie){
-        for(let column in barrie[row]){
+    for (let row in barrie) {
+        for (let column in barrie[row]) {
+
 
             let tile = barrie[row][column] //bloquinho
 
-            if(tile === 1){
-                let elementTile = newElement("div", "barrie")
+            if (tile === 1) {
+                numBarrie++
+                let elementTile = newElement("div", "barrie", `barrie${numBarrie}`)
                 elementTile.style.backgroundColor = "green"
                 this.gameContext.appendChild(elementTile)
-                
-            }else if(tile === 0){
+
+            } else if (tile === 0) {
                 let elementTile = newElement("div", "air")
                 elementTile.style.backgroundColor = "deepskyblue"
                 this.gameContext.appendChild(elementTile)
@@ -165,15 +169,15 @@ function DrawBarries(barrie){ // recebe a matrix da barreira específica (cenár
     gameArea.insertAdjacentElement('afterbegin', this.gameContext);
 
     this.getY = () => parseInt(this.gameContext.style.top.split('px')[0]);
-    this.setY = (y) =>  this.gameContext.style.top = `${y}px`;
+    this.setY = (y) => this.gameContext.style.top = `${y}px`;
 
-    this.animate = () =>{
+    this.animate = () => {
 
         const motion = 5 //acrescimo de pixels por movimento
         let newYposition = this.getY();
 
         newYposition = this.getY() + motion;
-        
+
         this.setY(newYposition)
 
         // if(this.getY() > 1000)
@@ -181,15 +185,15 @@ function DrawBarries(barrie){ // recebe a matrix da barreira específica (cenár
 
     }
 
-    this.delete = () =>{
+    this.delete = () => {
         this.gameContext.parentNode.removeChild(this.gameContext)
     }
 
 }
 
-function SortBarries(barries){ // recebe o objeto que contem todas as barreiras (cenários)
+function SortBarries(barries) { // recebe o objeto que contem todas as barreiras (cenários)
 
-    this.arrBarries = [ 
+    this.arrBarries = [
         barries.bifurcation,
         barries.zigzag,
         barries.midle,
@@ -197,32 +201,29 @@ function SortBarries(barries){ // recebe o objeto que contem todas as barreiras 
         barries.toleft
     ]
 
-    return this.arrBarries[Math.floor(Math.random()*this.arrBarries.length)] // retorna uma barreira aleatória
-
-
-
+    return this.arrBarries[Math.floor(Math.random() * this.arrBarries.length)] // retorna uma barreira aleatória
 }
 
 function Cenary() {
     const barries = new Barries()
-    
-    this.newCenary = () =>{
+
+    this.newCenary = () => {
         return new DrawBarries(SortBarries(barries))
     }
 
     let newCenary = this.newCenary()
     let oldCenary
 
-    
-    this.animate = () =>{ 
+
+    this.animate = () => {
         newCenary.animate()
-        
-        if(typeof oldCenary === 'object' && oldCenary !== null){
+
+        if (typeof oldCenary === 'object' && oldCenary !== null) {
             oldCenary.animate()
-            if(oldCenary.getY() == 680) oldCenary.delete()
+            if (oldCenary.getY() == 680) oldCenary.delete()
         }
 
-        if(newCenary.getY() == 0 ){
+        if (newCenary.getY() == 0) {
             oldCenary = newCenary
             newCenary = this.newCenary()
         }
@@ -231,51 +232,51 @@ function Cenary() {
 
 }
 
-function Ship(){
+function Ship() {
 
     this.goingRight = false
     this.goingLeft = false
-    
-    this.element = newElement('img', 'ship')
+
+    this.element = newElement('img', 'ship', 'myShip')
     this.element.src = 'imgs/nave.png'
     this.element.style.right = "500%"
 
     this.getX = () => parseInt(this.element.style.right.split('px')[0])
     this.setX = (x) => this.element.style.right = `${x}px`
 
-    document.addEventListener("keydown", (event) =>{
-        switch(event.key){
+    document.addEventListener("keydown", (event) => {
+        switch (event.key) {
             case "ArrowLeft":
                 this.goingLeft = true
                 break
-            
+
             case "ArrowRight":
                 this.goingRight = true
                 break
-        }   
+        }
     });
-    
-    document.addEventListener("keyup", (event) =>{
-        switch(event.key){
+
+    document.addEventListener("keyup", (event) => {
+        switch (event.key) {
             case "ArrowLeft":
                 this.goingLeft = false
                 break
-            
+
             case "ArrowRight":
                 this.goingRight = false
                 break
-        }   
+        }
     });
 
 
     this.animate = () => {
-        
+
         const motion = 15 //acrescimo de pixels por movimento
         let newXposition = this.getX()
 
-        if(this.goingRight){
+        if (this.goingRight) {
             newXposition = this.getX() - motion;
-        }else if(this.goingLeft){
+        } else if (this.goingLeft) {
             newXposition = this.getX() + motion;
         }
         this.setX(newXposition)
@@ -283,23 +284,53 @@ function Ship(){
 
 }
 
-function RiverRaid(){
+function Collid() {
+
+    this.isColliding = () => {
+
+        let shipElement = document.getElementById("myShip")
+        let barriesLenght = document.getElementsByClassName("barrie").length
+
+        for (let i = 0; i < barriesLenght; i++) {
+            let barrieElement = document.getElementsByClassName("barrie")[i]
+            const a = shipElement.getBoundingClientRect()
+            const b = barrieElement.getBoundingClientRect()
+            const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left
+            const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top
+
+            if(horizontal && vertical)
+                return true
+        }
+      return false  
+
+    }
+
+    this.verifyCollid = () => {
+
+        if (this.isColliding())
+            alert("colidiu!")
+
+    }
+
+}
+
+
+function RiverRaid() {
 
     const cenary = new Cenary()
     const ship = new Ship()
-    // const barries = new Barries()
-    // const drawBarries = new DrawBarries(SortBarries(barries))
-        
 
     gameArea.appendChild(ship.element)
 
-    this.start = () => {
+    const collid = new Collid()
 
-        const timer = setInterval(() =>{
-            
+    this.start = () => {
+        const timer = setInterval(() => {
+
             ship.animate()
             cenary.animate()
-            
+            collid.verifyCollid()
+
         }, 20)
 
     }
